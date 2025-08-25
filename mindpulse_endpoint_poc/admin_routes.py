a2a1cf1cdcc406826d0245da6781c64816c6d90b
Nate Vack <njvack@wisc.edu>
2025-08-25 18:16:08 -0500

    Add search

--- mindpulse_endpoint_poc/admin_routes.py
+++ mindpulse_endpoint_poc/admin_routes.py
@@ -3,19 +3,46 @@ Admin routes for the app
 """
 
 import logging
-from flask import url_for, redirect, render_template
+import re
+
+from flask import url_for, redirect, render_template, request, flash
 
 from .models import enrollment_key
 
 def register(app):
+    enrollment_key.logger = app.logger
     app.logger.debug("Registering admin routes...")
     
     @app.get("/enrollments")
     def enrollments_form():
         return render_template('enrollments.html')
-        
+
+
+    @app.get("/enrollments/<key>")
+    def show_enrollment(key):
+        return ''
+
+    
+    @app.get("/enrollments/search")
+    def search_enrollments():
+        keys_path = app.config['KEYS_PATH']
+        key = None
+        try:
+            key = enrollment_key.EnrollmentKey.load_for_search_str(
+                keys_path,
+                request.args.get('q', '')
+            )
+            app.logger.info(key)
+            return redirect(url_for('show_enrollment', key=key.hexdata))
+        except Exception as e:
+            flash('Enrollment not found')
+            app.logger.error(e)
+        return redirect(url_for('enrollments_form'))
+
+
     @app.post("/enrollments")
     def create_enrollment():
         k = enrollment_key.EnrollmentKey.generate_and_persist_random(app.config['KEYS_PATH'])
         app.logger.debug(f"Generated key {k.hexdata} with short_sha {k.short_sha}")
         return redirect(url_for('enrollments_form'))
+
--- mindpulse_endpoint_poc/models/enrollment_key.py
+++ mindpulse_endpoint_poc/models/enrollment_key.py
@@ -7,7 +7,12 @@ These are AES256 keys, which are generally persisted to the filesystem.
 
 from dataclasses import dataclass
 import hashlib
+import re
 import secrets
+
+import logging
+logger = logging.getLogger(__name__)
+
 
 SHORT_SHA_LEN=8
 KEY_LEN=32
@@ -35,20 +40,47 @@ class EnrollmentKey:
     @classmethod
     def load_for_short_sha(kls, keys_path, short_sha):
         infile = keys_path / f"{short_sha}.key"
+        logger.debug(f"Trying to read {infile}")
         return kls(hexdata=infile.read_text().strip().lower())
     
     @classmethod
+    def load_for_search_str(kls, keys_path, search_str):
+        # search_str should either be an 8-hexchar shortsha or a 64-hexchar key
+        search_norm_unsafe = search_str.strip().lower()
+        logger.debug(f"{search_norm_unsafe=}")
+        search_filtered = re.sub(r'[^0-9a-f]+', '', search_norm_unsafe)
+        logger.debug(f"{search_filtered=}")
+        kb = keys_path.resolve()
+        key_file = kb / f"{search_filtered}.key"
+        logger.debug(f"Key file is: {key_file}")
+        key_file.relative_to(kb)
+        if key_file.exists():
+            return kls.load_for_short_sha(keys_path, search_filtered)
+        # okay maybe it's a key
+        short_sha = short_sha_for_hex(search_filtered)
+        # I _know_ this is a safe string because it's from a hash
+        # If file doesn't exist we'll raise a FileNotFoundError, that's fine
+        return kls.load_for_short_sha(keys_path, short_sha)
+        
+    @classmethod
     def generate_random(kls):
         return kls(hexdata=secrets.token_hex(KEY_LEN))
-
+    
+    
     @property
     def short_sha(self):
-        m = hashlib.sha256()
-        key_bytes = bytes.fromhex(self.hexdata)
-        m.update(key_bytes)
-        digest = m.hexdigest()
-        return digest[0:SHORT_SHA_LEN]
+        return short_sha_for_hex(self.hexdata)
+
+def short_sha_for_hex(hex_str):
+    m = hashlib.sha256()
+    key_bytes = bytes.fromhex(hex_str)
+    m.update(key_bytes)
+    digest = m.hexdigest()
+    return digest[0:SHORT_SHA_LEN]
 
 
 class UniqueGenerationError(RuntimeError):
+    pass
+
+class PathError(ValueError):
     pass
